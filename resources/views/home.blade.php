1cd0f2653d3af734f646bdb7facf86dc2c56dc8b
unknown <honarmandpooria@gmail.com>
2019-07-05 21:18:47 +0430

    when no orders added there is a good page with park picture is showing to customers.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,20 @@
 @section('content')
     <div class="container">
 
-        <a href="{{route('customer-orders.create')}}" class="btn btn-success btn-block shadow"><i
+        <a href="{{route('customer-orders.create')}}" class="btn btn-success btn-block shadow my-2"><i
                 class="fas mx-2 fa-plus"></i>ثبت
             سفارش ترجمه</a>
 
 
-        <div dir="rtl" class="row mt-3">
+        <div dir="rtl" class="row">
 
 
             @if(count($orders->where('status_id',2)))
                 <div class="col-md-3 mb-2">
 
-                    <a class="text-dark" href="{{route('customer-orders.show',$orders->where('status_id',2)->first()->id)}}" style="text-decoration: none;">
+                    <a class="text-dark"
+                       href="{{route('customer-orders.show',$orders->where('status_id',2)->first()->id)}}"
+                       style="text-decoration: none;">
                         <div class="card bg-warning border-warning shadow">
 
                             <div class="card-body">
@@ -22,7 +24,8 @@
 
                                 <i class="fas fa-money-check fa-2x ml-2"></i>
                                 در انتظار پرداخت:
-                                <span class="persian-num font-weight-bolder mx-3" style="font-size: 1.5rem;">
+
+                                <span class="persian-num font-weight-bolder mx-3 float-left" style="font-size: 3rem;">
                             {{count($orders->where('status_id',2))}}
                         </span>
 
@@ -53,7 +56,8 @@
                         <div class="card-body text-white">
                             <i class="fas fa-recycle fa-2x ml-2"></i>
                             درحال بررسی:
-                            <span class="persian-num font-weight-bolder mx-3" style="font-size: 1.5rem;">
+
+                            <span class="persian-num font-weight-bolder mx-3 float-left" style="font-size: 3rem;">
                             {{count($orders->where('status_id',1))}}
                         </span>
                         </div>
@@ -83,7 +87,8 @@
                         <div class="card-body text-white">
                             <i class="fas fa-pen-alt fa-2x ml-2"></i>
                             درحال ترجمه:
-                            <span class="persian-num font-weight-bolder mx-3" style="font-size: 1.5rem;">
+
+                            <span class="persian-num font-weight-bolder mx-3 float-left" style="font-size: 3rem;">
                             {{count($orders->where('status_id',3))}}
                         </span>
                         </div>
@@ -112,7 +117,8 @@
                         <div class="card-body ">
                             <i class="fas fa-check fa-2x ml-2 text-success"></i>
                             ترجمه شده:
-                            <span class="persian-num font-weight-bolder mx-3 " style="font-size: 1.5rem;">
+
+                            <span class="persian-num font-weight-bolder mx-3 float-left" style="font-size: 3rem;">
                             {{count($orders->where('status_id',4))}}
                         </span>
                         </div>
--- resources/views/inc/order/table.blade.php
+++ resources/views/inc/order/table.blade.php
@@ -1,39 +1,57 @@
-<hr>
+<div class="bg-white rounded shadow py-4 mt-3">
 
-<h4 class="mt-5 pb-0" dir="rtl"><i class="fas fa-list mx-3"></i>لیست ترجمه های من</h4>
 
-<div class="mx-md-5">
-    <table dir="rtl" class="table table-hover mt-3">
-        <thead>
-        <tr>
-            <th scope="col">زمان ثبت</th>
-            <th scope="col">وضعیت</th>
-            <th scope="col">مشاهده</th>
+    <div class="mx-md-5">
 
-        </tr>
-        </thead>
-        <tbody>
+
         @if(count(Auth::user()->orders) > 0)
-            @foreach($orders as $order)
 
-                <tr class="{{$order->status_id == 1 ? 'table-primary' : ($order->status_id == 2 ? 'table-warning' : ($order->status_id == 3 ? 'table-success' : 'table-light'))}}">
-                    <td>{{$order->created_at->diffForHumans()}}</td>
-                    <td class="translate">{{$order->status->name}}</td>
-                    <td>
-                        <a href="{{route('customer-orders.show',$order->id)}}"
-                           class="btn btn-info rounded-circle shadow"><i class="fas fa-eye text-white"></i></a>
-                    </td>
+            <h4 class="mt-3 pb-0" dir="rtl"><i class="fas fa-list mx-3"></i>لیست ترجمه های من</h4>
+
+            <table dir="rtl" class="table table-hover mt-3">
+                <thead>
+                <tr>
+                    <th scope="col">زمان ثبت</th>
+                    <th scope="col">وضعیت</th>
+                    <th scope="col">مشاهده</th>
 
                 </tr>
+                </thead>
+                <tbody>
+                @foreach($orders as $order)
 
+                    <tr class="{{$order->status_id == 1 ? 'table-danger' : ($order->status_id == 2 ? 'table-warning' : ($order->status_id == 3 ? 'table-success' : 'table-light'))}}">
+                        <td>{{$order->created_at->diffForHumans()}}</td>
+                        <td class="translate">{{$order->status->name}}</td>
+                        <td>
+                            <a href="{{route('customer-orders.show',$order->id)}}"
+                               class="btn btn-info rounded-circle shadow"><i class="fas fa-eye text-white"></i></a>
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>
             @endforeach
 
         @else
-            <tr>
-                <td>هنوز ترجمه ای توسط شما ثبت نشده است.</td>
-            </tr>
+
+
+
+            <div class="text-center">
+                <p dir="rtl" class="text-center">هنوز ترجمه ای توسط شما ثبت نشده است. روی <a class="text-success"
+                                                                                             href="{{route('customer-orders.create')}}">این
+                        لینک</a> کنید تا یک ترجمه
+                    ثبت کنید. شمارش کلمات و برآورد قیمت برای شما رایگان است!</p>
+                <img class="img-fluid" src="{{asset('images/tootiko/park.png')}}" alt="">
+
+
+            </div>
+
+
+
         @endif
 
-        </tbody>
-    </table>
+
+    </div>
+
 </div>
