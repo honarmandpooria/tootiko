dd60b40e10c9fc3cbc5f5b60030a4679ef5af924
unknown <honarmandpooria@gmail.com>
2019-07-06 17:48:45 +0430

    model relations for ticket, message and TicketStatus has been clarified.

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+        public function ticketStatus(){
+        return $this->belongsTo(TicketStatus::class);
+    }
+
+
+
+
 }
